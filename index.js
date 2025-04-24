2cadef8437bd65114df6fa6c9239d8119738f745
Azpil <eazpil01@gmail.com>
2025-04-24 13:43:06 -0600

    I add the country

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const port = 3000;
 
 app.get("/", (req, res) => {
     const data = {
-        theName: "Juan E",
-        theAge: 37,
         theCountry: "MX"
     }
     res.render("index.ejs", data);
