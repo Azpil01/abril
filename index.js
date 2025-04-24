0d57ddb255c264dca0c92e27ef9a3bf5e0a62843
Azpil <eazpil01@gmail.com>
2025-04-23 20:56:10 -0600

    I add the first files

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = new express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+app.listen(port, (req, res) => {
+    console.log(`All ok from ${port} Azpil!`);
+})
